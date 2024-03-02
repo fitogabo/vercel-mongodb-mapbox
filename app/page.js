96219c882b00a0a16b3a09b9a15f0e1663359b6d
fitogabo <gapantoj@uc.cl>
2024-03-02 03:39:56 -0300

    Refactor code: Remove unused state and API call

--- app/page.js
+++ app/page.js
@@ -1,25 +1,11 @@
-import { useState } from 'react';
+// app/page.js
 import Map from './map/page.js';
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
-
-  const refreshData = async () => {
-    setLoading(true);
-    const res = await fetch('/api/getMarkers');
-    const newData = await res.json();
-    setData(newData);
-    setLoading(false);
-  };
-
   return (
     <main className="w-full h-full">
       <h1>Mapa Referencial</h1>
-      <button onClick={refreshData} disabled={loading}>
-        {loading ? 'Cargando...' : 'Refrescar base de datos'}
-      </button>
-      <Map data={data} />
+      <Map />
     </main>
   );
 }
