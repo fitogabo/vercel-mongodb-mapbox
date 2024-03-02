ddeb707ca615cf67e549f3a4ddc1c7563bd72322
fitogabo <gapantoj@uc.cl>
2024-03-02 03:16:47 -0300

    Add heading to home page

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Map from './map/page.js';
 export default function Home() {
   return (
     <main className="w-full h-full">
+      <h1>Mapa Referencial</h1>
       <Map />
     </main>
   );
