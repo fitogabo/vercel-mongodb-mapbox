aab5350db3cfaeee099d81122b9691ce0bf6f1c3
fitogabo <gapantoj@uc.cl>
2024-03-06 01:25:51 -0300

    Add data fetching for markers in Home component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
 // app/page.js
+import React, { useEffect, useState } from 'react';
 import Map from './map/page.js';
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/getMarkers')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <main className="w-full h-full">
       <h1>Mapa Referencial</h1>
-      <Map />
+      <Map data={data} />
     </main>
   );
 }
